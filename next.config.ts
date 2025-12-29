64c0f6fcff05005067f0875c173ef78848cb2d93
tipin513 <josevi3000@gmail.com>
2025-12-29 03:32:26 -0300

    Simplificar configuracion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Empty config - relying on page-level dynamic exports
 };
 
 export default nextConfig;
